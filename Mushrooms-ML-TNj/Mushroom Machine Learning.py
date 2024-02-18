a62b7737566f5b97d4be1c3e82a3da150f43313b
NicholasSchaus <nicholas.schaus@mnsu.edu>
2024-02-17 23:18:44 -0600

    ML algorithm start

--- Mushrooms-ML-TNj/Mushroom Machine Learning.py
+++ Mushrooms-ML-TNj/Mushroom Machine Learning.py
@@ -16,7 +16,7 @@ from sklearn import tree
 #  'stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']
 rawDataFilePath= os.getcwd() + "\\mushroom\\agaricus-lepiota.csv"
 
-def FeatureSeparation(rawDataFilePath):
+def FeatureSeparation(rawDataFilePath, printToCSV=False):
 # Adjust file path, can remove getcwd if easier and just put file path in parenthesis. *Must convert to .csv first*
     rawData = read_csv(filepath_or_buffer=rawDataFilePath, index_col=False, dtype=str)
     # rawData['stalk-root'].replace(to_replace="?", value=nan, inplace=True)
@@ -45,112 +45,139 @@ def FeatureSeparation(rawDataFilePath):
         for j in range(len(FeatureRawValues[i])):
             FeatureListToTokenMap.append(FeatureRawValues[i][j])
 
-    Target = rawData[rawData.keys()[0]]
+    Target = rawData[rawData.keys()[0]].replace(to_replace={'p': 1,
+                                                            'e': 0})
     rawData.drop('classification', axis='columns')
     Data = pandas.DataFrame(index=range(len(rawData[rawData.keys()[0]])), columns=FeatureMap, dtype=bool).replace(to_replace=True, value=False)
+    Data = Data.astype(dtype=int, copy=True)
 
     for col in range(len(FeatureListToTokenMap)):
         for ind in range(len(rawData[rawData.columns[0]])):
             if(rawData.at[ind,FeatureListToKeyMap[col]] == FeatureListToTokenMap[col]):
-                Data.at[ind,FeatureMap[col]] = True
+                Data.at[ind,FeatureMap[col]] = 1
+    if printToCSV:
+        df = pandas.DataFrame(Data)
+        df.insert(loc=len(Data.columns), column='Target', value=Target)
+        df.to_csv(path_or_buf=os.getcwd()+"\\mushroom\\FeatureSelectedData.csv")
 
     return Data, Target, FeatureMap, FeatureCounts, FeatureRawValues
 
-# Data.to_csv(path_or_buf=os.getcwd() + "\\mushroom\\FeatureSelected.csv")
-
-Data, Target, FeatureMap, FeatureCounts, FeatureRawValues = FeatureSeparation(rawDataFilePath)
 
 
+def ShowGraphs():
+    # Correlation Matrix
+    correlationMatrix = Data.corr()
+    plt.matshow(correlationMatrix)
+    cb = plt.colorbar()
+    cb.ax.tick_params(labelsize=6)
+    plt.xticks(ticks=range(len(FeatureMap)), labels=FeatureMap, fontsize=6, rotation=90)
+    plt.yticks(ticks=range(len(FeatureMap)), labels=FeatureMap, fontsize=6)
+    plt.show()
 
-
-# Correlation Matrix
-correlationMatrix = Data.corr()
-plt.matshow(correlationMatrix)
-cb = plt.colorbar()
-cb.ax.tick_params(labelsize=6)
-plt.xticks(ticks=range(len(FeatureMap)), labels=FeatureMap, fontsize=6, rotation=90)
-plt.yticks(ticks=range(len(FeatureMap)), labels=FeatureMap, fontsize=6)
-plt.show()
-
-#Box plot graphs of data
-gridSize = int(int(len(FeatureCounts)**.5)+1)
-for j in range(gridSize):
-    for l in range(gridSize):
-        i = gridSize*j +l
-        try:
-            FeatureCounts[i]
-            plt.subplot2grid((gridSize, gridSize), (j, l), rowspan=1, colspan=1) 
-            plt.bar(x=range(len(FeatureCounts[i])), height=FeatureCounts[i], align='center', bottom=0)
-            plt.xticks(ticks=range(len(FeatureCounts[i])), labels=FeatureRawValues[i])
-            plt.title(Data.keys()[i])
-        except IndexError:
-            plt.cla()
-plt.subplots_adjust(left=.025, right=.79, bottom=.25, top=.97, hspace=.9, wspace=.6)
-plt.get_current_fig_manager().full_screen_toggle()
-plt.show()
+    #Box plot graphs of data
+    gridSize = int(int(len(FeatureCounts)**.5)+1)
+    for j in range(gridSize):
+        for l in range(gridSize):
+            i = gridSize*j +l
+            try:
+                FeatureCounts[i]
+                plt.subplot2grid((gridSize, gridSize), (j, l), rowspan=1, colspan=1) 
+                plt.bar(x=range(len(FeatureCounts[i])), height=FeatureCounts[i], align='center', bottom=0)
+                plt.xticks(ticks=range(len(FeatureCounts[i])), labels=FeatureRawValues[i])
+                plt.title(Data.keys()[i])
+            except IndexError:
+                plt.cla()
+    plt.subplots_adjust(left=.025, right=.79, bottom=.25, top=.97, hspace=.9, wspace=.6)
+    plt.get_current_fig_manager().full_screen_toggle()
+    plt.show()
 
 
 
 
-# from sklearn.feature_selection import RFECV 
-# # import !gridsearch! 
-# from sklearn.feature_selection import RFE
-# from sklearn.linear_model import LinearRegression
-# from sklearn.tree import DecisionTreeRegressor
-# from sklearn.tree import DecisionTreeClassifier
-# import matplotlib.pyplot as plt
-# import multiprocessing as mp
-# import concurrent.futures as conc
+from sklearn import metrics as metrics
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn import metrics
 
 
-# # GridSearch: Look up
+def RFRunner(rawData, filePath=None):
+    if not (filePath == None):
+        rawData = read_csv(filepath_or_buffer=filePath, dtype=int, na_filter=False, index_col=0)
+    X = rawData[rawData.columns[:len(rawData.columns)-1]]
+    y = rawData.get(rawData.columns[len(rawData.columns)-1])
+    print(y.shape)
+    RandomForestClassifier().get_params()
+
+    n_estimators_list = list(range(10,len(X.columns),20))
+    criterion_list = ['gini', 'entropy']
+    max_depth_list = [2, 3, 4, 5, 7, 9]
+    temp = list(range(10,int(len(X.columns)),5))
+    max_depth_list.append(temp)
+    min_samples_split_list = [x/1000 for x in list(range(5, len(X.columns)%10+1, 10))]
+    min_samples_leaf_list = [x/1000 for x in list(range(5, len(X.columns)%10+1, 10))]
+    max_features_list = ['sqrt', 'log2']
+
+    params_grid = {
+        'n_estimators': n_estimators_list,
+        'criterion': criterion_list,
+        'max_depth': max_depth_list,
+        'min_samples_split': min_samples_split_list,
+        'min_samples_leaf': min_samples_leaf_list,
+        'max_features': max_features_list
+    }
+
+    num_combinations = 1
+    for k in params_grid.keys(): num_combinations *= len(params_grid[k])
+
+    print('Number of combinations = ', num_combinations)
+    params_grid
+    print("IGNORE FOLLOWING ERRORS (NO IMPACT ON PROGRAM), OR FIND CAUSE (RELATED TO ShowGraphs())")
+
+    def my_roc_auc_score(model, X, y): return metrics.roc_auc_score(y, model.predict(X))
+    model_rf = GridSearchCV(estimator=RandomForestClassifier(class_weight='balanced'),
+                            param_grid=params_grid,
+                            n_jobs=3,
+                            cv=3,
+                            scoring=my_roc_auc_score,
+                            return_train_score=True,)
+    # model_rf = RandomizedSearchCV(estimator=RandomForestClassifier(class_weight='balanced'),
+    #                               param_distributions=params_grid,
+    #                               n_iter=50,
+    #                               cv=3,
+    #                               scoring=my_roc_auc_score,
+    #                               return_train_score=True,
+    #                               verbose=2)
+
+    model_rf.fit(X,y)
+
+    print(model_rf.best_params_)
+
+    df_cv_results = pandas.DataFrame(model_rf.cv_results_)
+    df_cv_results = df_cv_results[['rank_test_score','mean_test_score','mean_train_score',
+                            'param_n_estimators', 'param_min_samples_split','param_min_samples_leaf',
+                            'param_max_features', 'param_max_depth','param_criterion']]
+    df_cv_results.sort_values('rank_test_score', inplace=True)
+    print(df_cv_results['mean_test_score'])
+
+    print(df_cv_results[:20])
+   
 
 
-# outputs = {}
 
-# X = IntMappedData.drop(["classification"], axis = 'columns')
-# y = IntMappedData["classification"]
 
-# def func(X, y, pid):
-#     print(pid)
-#     regressor = DecisionTreeRegressor(max_depth=5, min_samples_split=2)
-#     feature_selector = RFE(regressor, step=2)
 
-#     fit = feature_selector.fit(X,y)
 
-#     ranking = feature_selector.ranking_
-#     optimal_feature_count = feature_selector.n_features_
-#     outputs[pid] = (optimal_feature_count, ranking)
 
-# # with conc.ThreadPoolExecutor(max_workers=1) as executor:
-# #     future = executor.submit(processFunction, X, y)
-# #     print(future.result())
 
-# proc = []
-# for ii in range(5)[1:]:
-#     for jj in range(5)[2:]:
-#         for kk in range(5)[1:]:
-#             for ll in range(22)[1:]:
-#                 p = mp.Process(target=func, args=(X, y, (ii, jj, kk, ll), ii, jj, kk, ll))
-#                 p.run()
-#                 proc.append(p)
-# print("Here!")
-# for i in proc:
-#     try:
-#         if(not i.is_alive()):
-#             i.join()
-#     except AssertionError:
-#         pass
-# print("Finished!")
+# Data, Target, FeatureMap, FeatureCounts, FeatureRawValues = FeatureSeparation(rawDataFilePath, printToCSV=True)
+# ShowGraphs()
 
-# for i in outputs.keys():
-#     print(outputs[i])
 
-# # # X_new = X.loc[:, feature_selector.get_support()]
+featureSelectedDataFilePath = os.getcwd() + "\\mushroom\\FeatureSelectedData.csv"
+rawData = read_csv(filepath_or_buffer=featureSelectedDataFilePath, dtype=int, na_filter=False, index_col=0)
 
-# # # plt.plot(range(1, len(fit.ranking_) + 1), fit., marker = "o")
-# # # plt.ylabel("Model Score")
-# # # plt.xlabel("Number of Features")
-# # # plt.title(f"Feature Selection using RFE \n Optimal number of features is {optimal_feature_count} (at score of {round(max(fit.ranking_), 4)})")
-# # # plt.tight_layout() 
-# # # plt.show()
+RFRunner(rawData=rawData)
+
+
+
